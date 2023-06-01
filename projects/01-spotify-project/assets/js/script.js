bbb3c77b21ee31b10b8ce59cb6f880af8df25e6f
Bruno Victor Lima <101837219+brunovictorlima@users.noreply.github.com>
2023-06-01 00:22:44 -0300

    Atualização em Projetos
    Projeto Spotify Clone foi aprimorado.

---
+++ projects/01-spotify-project/assets/js/script.js
@@ -0,0 +1,7 @@
+const menuButton = document.querySelector('.menu-button');
+const menu = document.querySelector('#menu');
+
+menuButton.addEventListener('click', () => {
+    menuButton.classList.toggle('active');
+    menu.classList.toggle('active');
+});
